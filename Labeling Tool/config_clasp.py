460a62d387a9a5c20c1564d6ea1afe3ab898c2e8
Yuexiaoxi10 <zhang.yuexi10@gmail.com>
2018-05-05 18:24:43 -0400

    add files

---
+++ Labeling Tool/config_clasp.py
@@ -0,0 +1,110 @@
+LABELS = (
+    {
+        'attributes': {
+            'class':      'Person',
+         },
+        'inserter': 'sloth.items.RectItemInserter',
+        'item':     'sloth.items.RectItem',
+        'hotkey':   'q',
+        'text':     'Person',
+    },
+    {
+        'attributes': {
+            'class':      'BinFULL',
+         },
+        'inserter': 'sloth.items.RectItemInserter',
+        'item':     'sloth.items.RectItem',
+        'hotkey':   'w',
+        'text':     'Bin Full',
+    },
+    {
+        'attributes': {
+            'class':      'BinEMPTY',
+         },
+        'inserter': 'sloth.items.RectItemInserter',
+        'item':     'sloth.items.RectItem',
+        'hotkey':   'e',
+        'text':     'Bin Empty',
+    },
+    {
+        'attributes': {
+            'class':    'Left Hand',
+        },
+        'inserter': 'sloth.items.PointItemInserter',
+        'item':     'sloth.items.PointItem',
+        'hotkey':   'a',
+        'text':     'Left Hand',
+    },
+    {
+        'attributes': {
+            'class':    'Left Elbow',
+        },
+        'inserter': 'sloth.items.PointItemInserter',
+        'item':     'sloth.items.PointItem',
+        'hotkey':   's',
+        'text':     'Left Elbow',
+    },
+    {
+        'attributes': {
+            'class':    'Left Shoulder',
+        },
+        'inserter': 'sloth.items.PointItemInserter',
+        'item':     'sloth.items.PointItem',
+        'hotkey':   'd',
+        'text':     'Left Shoulder',
+    },
+    {
+        'attributes': {
+            'class':    'Right Hand',
+        },
+        'inserter': 'sloth.items.PointItemInserter',
+        'item':     'sloth.items.PointItem',
+        'hotkey':   'z',
+        'text':     'Right Hand',
+    },
+    {
+        'attributes': {
+            'class':    'Right Elbow',
+        },
+        'inserter': 'sloth.items.PointItemInserter',
+        'item':     'sloth.items.PointItem',
+        'hotkey':   'x',
+        'text':     'Right Elbow',
+    },
+    {
+        'attributes': {
+            'class':    'Right Shoulder',
+        },
+        'inserter': 'sloth.items.PointItemInserter',
+        'item':     'sloth.items.PointItem',
+        'hotkey':   'c',
+        'text':     'Right Shoulder',
+    },
+    {
+        'attributes': {
+            'class':    'Head',
+        },
+        'inserter': 'sloth.items.PointItemInserter',
+        'item':     'sloth.items.PointItem',
+        'hotkey':   'f',
+        'text':     'Head',
+    },
+    {
+          'attributes': {
+            'class':    'Action_PutIn',
+          },
+          'inserter': 'sloth.items.RectItemInserter',
+          'item':     'sloth.items.RectItem',
+          'hotkey':   'r',
+          'text':     'Action_PutIn',
+    },
+    {
+          'attributes': {
+            'class':    'Action_PutOut',
+          },
+          'inserter': 'sloth.items.RectItemInserter',
+          'item':     'sloth.items.RectItem',
+          'hotkey':   't',
+          'text':     'Action_PutOut',
+    },
+)
